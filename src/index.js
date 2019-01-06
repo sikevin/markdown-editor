6df0c62d0d2edee6b8cb84747a5deea21e884407
Kevin SI <sikevin.sk@gmail.com>
2019-01-06 16:00:01 +0100

    added states to update both parts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-// Ici on code notre App
+import React from 'react';
+import {render} from 'react-dom';
+
+// Styles
+import './style/css/bootstrap.min.css';
+import './index.css';
+
+// JS Perso
+
+import {sampleText} from './sampleText'
+
+class App extends React.Component{
+	
+	state = {
+		text: sampleText
+	};
+
+	editText = (event) => {
+		const text = event.target.value;
+		this.setState({text});
+	};
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+
+					<div className="col-sm-6">
+						<textarea 
+							rows="35" 
+							className="form-control" 
+							value={this.state.text} 
+							onChange={(e) => this.editText(e)}
+						>
+							
+						</textarea>
+					</div>
+
+
+					<div className="col-sm-6">
+						<h1>Résultat</h1>
+						<p>{this.state.text}</p>
+					</div>
+
+				</div>
+			</div>
+		)
+	}
+}
+
+render(
+	<App />,
+	document.getElementById('root')
+);
